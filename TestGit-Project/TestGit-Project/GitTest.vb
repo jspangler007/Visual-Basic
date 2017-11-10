b5c1111034e76c549a3fe53ea3ecfe731fb99ea3
Joe Spangler <jspangler007@gmail.com>
2017-11-10 11:55:18 -0500

    Renamed form1 to GitTest

--- TestGit-Project/TestGit-Project/GitTest.vb
+++ TestGit-Project/TestGit-Project/GitTest.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form1
+﻿Public Class GitTest
     Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
         lblMessage.Text = "Hello World!"
     End Sub
--- TestGit-Project/TestGit-Project/My Project/Application.Designer.vb
+++ TestGit-Project/TestGit-Project/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TestGit_Project.Form1
+            Me.MainForm = Global.TestGit_Project.GitTest
         End Sub
     End Class
 End Namespace
